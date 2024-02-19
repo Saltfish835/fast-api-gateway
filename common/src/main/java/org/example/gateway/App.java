5e49eff5d2f192e7e5e445ee2f04944b8a70f5ef
yuhe <yuhe@hbut.edu.cn>
2024-02-19 15:22:00 +0800

    项目框架搭建

---
+++ common/src/main/java/org/example/gateway/App.java
@@ -0,0 +1,13 @@
+package org.example.gateway;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
